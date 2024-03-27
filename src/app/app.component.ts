f94d7ebffbc834a01cc7afdbab9da3380469df76
David.Marchionini <david.marchionini@verkor.com>
2024-03-27 17:42:34 +0100

    Add WIP google web auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,4 +23,8 @@ export class AppComponent {
     this.router.navigate(['']);
     this.authService.signOut();
   }
+
+  async googleLogOut() {
+    await this.authService.googleSignOut();
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable, inject } from "@angular/core";
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, sendEmailVerification, sendPasswordResetEmail, user } from "@angular/fire/auth";
-import { firstValueFrom } from 'rxjs';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, sendEmailVerification, sendPasswordResetEmail, User, user } from "@angular/fire/auth";
+import { firstValueFrom, Observable, Subscription } from 'rxjs';
+import { GoogleAuth, User as gUser } from "@codetrix-studio/capacitor-google-auth";
+import { isPlatform } from "@ionic/angular";
 import { UserService } from "./user.service";
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +12,16 @@ import { UserService } from "./user.service";
 export class AuthService {
   private readonly auth = inject(Auth);
   private readonly userService = inject(UserService);
+  googleUser: gUser | null = null;
 
   user$ = user(this.auth);
+
+  constructor() {
+    if(!isPlatform("capacitor")) {
+      GoogleAuth.initialize();
+    }
+  }
+
 
   async createUser(email: string, username: string, password: string): Promise<void> {
     const usersFound = await firstValueFrom(this.userService.getByName(username));
@@ -37,5 +48,23 @@ export class AuthService {
 
   isConnected(): boolean {
     return !!this.auth.currentUser;
-  }
+   }
+
+   async googleSignIn() {
+    this.googleUser = await GoogleAuth.signIn();
+    if (this.googleUser) {
+      //TODO: register if doesn't exist else login firestore
+    }
+    console.log("google user:", this.googleUser);
+   }
+
+   async googleRefresh() {
+    const authCode = await GoogleAuth.refresh();
+    console.log("refresh", authCode);
+   }
+
+   async googleSignOut() {
+    await GoogleAuth.signOut();
+    this.googleUser = null;
+   }
 }
